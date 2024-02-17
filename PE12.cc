97ee42ab36d352dbde5ceeaa45458c54148985b4
robynskyrme <robynskyrme@gmail.com>
2024-02-17 11:34:35 +0000

    Project Euler 12 in C

---
+++ PE12.cc
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n = 1;
+    int t = 2;
+    
+    int factors = 0;
+    
+    int found = 0;
+    
+    while (found == 0) {
+        factors = 0;
+        t = n * (n+1)/2;
+        
+        int i;
+        for (i = 1; i <= (int)sqrt(t)+1; i++) {
+            if (t % i == 0) {
+                factors +=2;
+            }
+        }
+        
+        n++;
+        
+        if (factors > 499) {
+            found = 1;
+        }
+    }
+
+    printf("%d",t);
+    printf("\ndone");
+
+    return 0;
+}
